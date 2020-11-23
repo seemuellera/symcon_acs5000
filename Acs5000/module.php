e093583958228d85900db4a951f65f903199aff7
Andreas Seemueller <andreas.seemueller@gmail.com>
2020-11-24 00:45:52 +0100

    Initial commit

---
+++ Acs5000/module.php
@@ -0,0 +1,114 @@
+<?php
+
+    // Klassendefinition
+    class Acs5000 extends IPSModule {
+ 
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+ 
+            // Selbsterstellter Code
+        }
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            
+		// Diese Zeile nicht löschen.
+            	parent::Create();
+
+		// Properties
+		$this->RegisterPropertyString("Sender","SymconAcs5000");
+		$this->RegisterPropertyInteger("RefreshInterval",5);
+		$this->RegisterPropertyInteger("SnmpInstance",0);
+
+		// Variables
+		$this->RegisterVariableString("Hostname", "Hostname");
+
+
+		// Timer
+		$this->RegisterTimer("RefreshInformation", 0, 'ACS5000_RefreshInformation($_IPS[\'TARGET\']);');
+ 
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+    public function ApplyChanges() {
+
+		$newInterval = $this->ReadPropertyInteger("RefreshInterval") * 1000;
+		$this->SetTimerInterval("RefreshInformation", $newInterval);
+
+
+		// Diese Zeile nicht löschen
+		parent::ApplyChanges();
+	}
+
+
+	public function GetConfigurationForm() {
+
+        	
+		// Initialize the form
+		$form = Array(
+            		"elements" => Array(),
+					"actions" => Array()
+        		);
+
+		// Add the Elements
+		$form['elements'][] = Array("type" => "NumberSpinner", "name" => "RefreshInterval", "caption" => "Refresh Interval");
+		$form['elements'][] = Array("type" => "SelectInstance", "name" => "SnmpInstance", "caption" => "SNMP instance");
+
+		// Add the buttons for the test center
+        $form['actions'][] = Array("type" => "Button", "label" => "Refresh Overall Status", "onClick" => 'ACS5000_RefreshInformation($id);');
+
+		// Return the completed form
+		return json_encode($form);
+
+	}
+
+
+        /**
+	* Get the list of robots linked to this profile and modifies the Select list to allow the user to select them.
+        *
+        */
+    public function RefreshInformation() {
+
+		$oid_mapping_table['Hostname'] = '.1.3.6.1.4.1.2925.8.2.1';
+
+		foreach (array_keys($oid_mapping_table) as $currentIdent) {
+		
+			$this->UpdateVariable($currentIdent, $oid_mapping_table[$currentIdent]);
+		}
+	}
+	
+	protected function LogMessage($message, $severity = 'INFO') {
+		
+		if ( ($severity == 'DEBUG') && ($this->ReadPropertyBoolean('DebugOutput') == false )) {
+			
+			return;
+		}
+		
+		$messageComplete = $severity . " - " . $message;
+		
+		IPS_LogMessage($this->ReadPropertyString('Sender') . " - " . $this->InstanceID, $messageComplete);
+	}
+
+
+	protected function UpdateVariable($varIdent, $oid) {
+	
+		$oldValue = GetValue($this->GetIDForIdent($varIdent));
+		$newValue = $this->SnmpGet($oid);
+
+		if ($newValue != $oldValue) {
+
+			SetValue($this->GetIdForIdent($varIdent), $newValue);
+		}
+	}
+
+	protected function SnmpGet($oid) {
+	
+		$result = IPSSNMP_ReadSNMP($this->ReadPropertyInteger("SnmpInstance"), Array($oid));
+
+		return $result[$oid];
+	}
+
+}
